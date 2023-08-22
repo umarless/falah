58328b6cde87ea7d84940bc1ce99423238ba315d
umarless <umarlesss@gmail.com>
2023-08-22 18:28:29 +0530

    changed words

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Frappe</h1>
+      <h1>project deployment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
